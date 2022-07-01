06faacdd2f1a16095a1109b617222007594c55b0
Shashank <kumarshashank4777@gmail.com>
2022-07-01 18:47:43 +0530

    Updates

---
+++ precache-manifest.cac8e9f6dd97d57b992b3134c2f12d3a.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dc11e06ca8850a72baaf1205794d5e59",
+    "url": "/my-kepper-app/index.html"
+  },
+  {
+    "revision": "4b5442fb60e60e7d84ec",
+    "url": "/my-kepper-app/static/js/2.07b269b6.chunk.js"
+  },
+  {
+    "revision": "786f862d40b7e7f89afc",
+    "url": "/my-kepper-app/static/js/main.7fd3df68.chunk.js"
+  },
+  {
+    "revision": "ca0e79f257ad916a9a4a",
+    "url": "/my-kepper-app/static/js/runtime-main.58d2327c.js"
+  }
+]);
